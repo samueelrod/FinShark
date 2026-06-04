8221758f6cba6796d490e760fcef127448c8bf8f
samueelrod <srodriguesbortolin@gmail.com>
2026-06-04 19:16:18 -0300

    aula 9 - type narrowing

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,25 +1,23 @@
-import axios from "axios"
-import { CompanySearch } from "./company"
+import axios from "axios";
+import { CompanySearch } from "./company";
 
-
-interface SearchResponse{
-    data: CompanySearch[];
+export interface SearchResponse {
+  data: CompanySearch[];
 }
 
-export const SearchCompanies = async (query: string) => {
-    try {
-        const data = await axios.get<SearchResponse>(
-            `https://api.twelvedata.com/symbol_search?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-        );
-        return data.data.data;
-    }catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.log("Error message: ", error.message);
-            return error.message;
-        }
-        else{
-            console.log("Unexpected error: ", error);
-            return "An unexpected error has occurred";
-        }
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occured.";
     }
-}
+  }
+};
