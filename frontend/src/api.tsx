4e5f678e001d4bfeca08dd12c2e2baa308768cf5
samueelrod <srodriguesbortolin@gmail.com>
2026-06-06 13:27:38 -0300

    aula 24 - cashflow statement

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import {
   CompanyBalanceSheet,
+  CompanyCashFlow,
   CompanyIncomeStatement,
   CompanyKeyMetrics,
-  CompanyKeyRatios,
   CompanyProfile,
   CompanySearch,
 } from "./company";
@@ -66,6 +66,17 @@ export const getBalanceSheet = async (query: string) => {
   try {
     const data = await axios.get<CompanyBalanceSheet[]>(
       `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?limit=20&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getCashFlow = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyCashFlow[]>(
+      `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=100&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
